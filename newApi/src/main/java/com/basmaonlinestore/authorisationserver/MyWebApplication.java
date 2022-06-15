288115b0d616cf56223787708aba724749d9ceb5
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-06-15 23:50:42 +0100

    :pencil: updating seeder rule :pencil:

--- newApi/src/main/java/com/basmaonlinestore/authorisationserver/MyWebApplication.java
+++ newApi/src/main/java/com/basmaonlinestore/authorisationserver/MyWebApplication.java
@@ -1,6 +1,7 @@
 package com.basmaonlinestore.authorisationserver;
 
 import com.basmaonlinestore.authorisationserver.Seeders.*;
+import com.basmaonlinestore.authorisationserver.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,12 +14,15 @@ public class MyWebApplication {
     public static void main(String[] args) {
         ApplicationContext context =  SpringApplication.run(MyWebApplication.class, args);
         // Seeders
-        context.getBean(RoleSeeder.class).Seed();
-        context.getBean(SpecialisationSeeder.class).Seed(30);
-        context.getBean(AdminSeeder.class).Seed();
-        context.getBean(AdministrationSeeder.class).Seed(30);
-        context.getBean(ProblemSeeder.class).Seed(30);
-        context.getBean(ReportSeeder.class).Seed(30);
+        UserService userService =  context.getBean(UserService.class);
+        if(userService.getAll().size() == 0){
+            context.getBean(RoleSeeder.class).Seed();
+            context.getBean(SpecialisationSeeder.class).Seed(30);
+            context.getBean(AdminSeeder.class).Seed();
+            context.getBean(AdministrationSeeder.class).Seed(30);
+            context.getBean(ProblemSeeder.class).Seed(30);
+            context.getBean(ReportSeeder.class).Seed(30);
+        }
 
     }
 }
